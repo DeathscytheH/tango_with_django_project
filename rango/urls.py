f76ee09785a97501963d8f5f2375d7675d9bc4fc
deathscytheh <alejandro.deathscythe@gmail.com>
2017-02-15 15:52:56 -0600

    Ejercicios del chap 4
    Se creo una nueva vista para el about page.
    
    Se mapeo el about page en el archivo urls.py.
    
    Y se modifico las respuestas de las vistas para incluir ligas a home y
    al about.

---
+++ rango/urls.py
@@ -0,0 +1,11 @@
+# Como se dijo que la app manejaria las vistas, se creo este archivo. Aqui se
+# manejaran los mapeos de las direcciones dentro de la app. Esto con el
+# objetivo de que sea modular
+
+from django.conf.urls import url
+from rango import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^about/', views.about, name='about'),
+]
--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hello!")
+    return HttpResponse("Rango says hello! Check our <a href='/rango/about'>about</a> page")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. Retun to <a href='/'> home </a> page.")
