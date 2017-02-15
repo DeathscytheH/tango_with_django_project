b1600fb0a354c610095cdd2cf31f9b6e129e7f5b
deathscytheh <alejandro.deathscythe@gmail.com>
2017-02-15 16:57:36 -0600

    Agregado  el directorio de Templates
    Se creo el directorio para los templates.
    En settings.py se agrego el directorio de los templates.
    Se creo el template para index.
    Se agrego a las vistas el template para el index.

--- rango/views.py
+++ rango/views.py
@@ -5,8 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hello! Check our <a href='/rango/about'>about</a> page")
+    # Construct a dictionary to pass to the template engine as it context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, full, sugar!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. Retun to <a href='/'> home </a> page.")
+    return HttpResponse("Rango says here is the about page. Retun to <a href='/rango'> home </a> page.")
