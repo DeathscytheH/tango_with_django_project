db7b1960511dad31c168614f122a0937395ce081
deathscytheh <alejandro.deathscythe@gmail.com>
2017-02-15 19:32:36 -0600

    Creación de los modelos
    Se crearon los primeros modelos.
    Categorias, y paginas.

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    """docstring for Category.
+    Category model for db
+    """
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    """docstring for Page.
+    Page model for db
+    """
+    category = models.ForeignKey(Category)
+    title = models.CharField(mac_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
