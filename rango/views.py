1cd216456db60ce3b04973b740480253ecc1305f
deathscytheh <alejandro.deathscythe@gmail.com>
2017-02-16 13:42:54 -0600

    Mostrar categorías en la pagina principal
    Se actualizo en views.py la función para mostrar las cinco primeras
    categorías en orden de mayor a menor.
    El template se actualizo para mostrar estos cambios.

--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Import the category model
+from rango.models import Category
+
 
 # Create your views here.
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as it context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, full, sugar!"}
+    # Query the db for a list of ALL categories currently stored. Order the
+    # categories by # of likes in descending order. Retrieve the top 5 only -
+    # or all if less than 5. Place the list in our context_dict that will be
+    # passed to the template engine.
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    # Render the response and send it back.
     return render(request, 'rango/index.html', context=context_dict)
 
 
