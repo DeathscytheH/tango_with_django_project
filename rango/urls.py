99ab8c3012c409b213581019248b7314322991b4
deathscytheh <alejandro.deathscythe@gmail.com>
2017-02-19 01:09:39 -0600

    Agregado parameter category name slug en urls.py
    Se agrego una dirección parametrizada utilizando regex

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,7 @@
 # Como se dijo que la app manejaria las vistas, se creo este archivo. Aqui se
 # manejaran los mapeos de las direcciones dentro de la app. Esto con el
 # objetivo de que sea modular
+# Modificamos la url de categoria para pasar el parametro category_name_slug
 
 from django.conf.urls import url
 from rango import views
@@ -8,5 +9,6 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-    url(r'^category/', views.show_category, name='category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
 ]
