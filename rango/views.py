8b5886c7e143a813e962e0931704c979b7c80ebe
deathscytheh <alejandro.deathscythe@gmail.com>
2017-03-14 01:51:55 -0600

    9.9 Logging out

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 
@@ -233,3 +233,13 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+
+# Use the login_required() decorator to ensure only those logged in can
+# access the view
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return HttpResponseRedirect(reverse('index'))
